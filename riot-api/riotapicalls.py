fac56fe84ef58b81f9449fe9ecd0e844a01e4508
Ahir Chatterjee <ahirchatterjee@gmail.com>
2019-12-21 01:29:33 -0600

    some old code that needed to be committed

--- riot-api/riotapicalls.py
+++ riot-api/riotapicalls.py
@@ -1,11 +1,31 @@
 import requests
 import json
 import os
+import time
 
+class apiCallList():
+    def __init__(self):
+        self.list = []  #filled with apiCalls, up to 200
+        self.API_RATE_LIMIT = 120   #API rate limit for our key type (standard access)
+        self.API_CALL_LIMIT = 200   #API call limit for our key type (standard access)
+    def makeCall(self):
+        if(len(self.list) == self.API_CALL_LIMIT-5):   #if we're at the API_CALL_LIMIT, then we need to evict the first call. A little less to be safe
+            now = time.time()
+            timeDelta = now-self.list[0]
+            if(timeDelta <= self.API_RATE_LIMIT+5):   #we can only evict the first call if it's been long enough, otherwise we need to wait. we add 5 seconds to be safe, though it does make it slower overall
+                print("about to wait on an api call for  " + (str)(timeDelta) + " seconds.")
+                time.sleep(timeDelta)
+                print("waited on an api call for " + (str)(timeDelta) + " seconds.")
+            self.list.pop(0)    #evict the first call since it's been long enough
+        self.list.append(time.time())
+        print(len(self.list))
+        
+        
 API_KEY = ""
+API_CALL_LIST = apiCallList()
 
 def saveFile(fileName, data, version):
-    overWrite = False
+    overwrite = False
     if(os.path.exists(fileName)):
         overwrite = True
     with open(fileName,'w') as outfile:
@@ -14,8 +34,7 @@ def saveFile(fileName, data, version):
         wrapperList.append(version)
         #format of wrapperList is [data,version]
         json.dump(wrapperList,outfile)
-
-    if(overWrite):
+    if(overwrite):
         print(fileName + " saved and overwritten successfully.")
     else:
         print(fileName + " saved successfully.")
@@ -36,23 +55,27 @@ def loadFile(fileName):
             wrapperList = json.loads(tempStr)  
         else:
             print(fileName + " is an empty file.")
+        openFile.close()
     else:
         print("Could not find a file named " + fileName + ".")
-        openFile.close()
     return wrapperList
 
 def getApiKey():
     """
     Either loads in the API_KEY or just returns the value if already loaded.
     """
+    global API_KEY
     if(len(API_KEY) == 0):
         fileName = "apikey.txt"
         if(os.path.exists("apikey.txt")):
             openFile = open(fileName,'r')
-            API_KEY = openFile.readLine()
-    return API_KEY
+            API_KEY = openFile.read()
+        else:
+            print("apikey.txt does not exist")
+    return "?api_key=" + API_KEY
 
 def getVersion():
+    API_CALL_LIST.makeCall()
     request = requests.get("https://ddragon.leagueoflegends.com/realms/na.json" + getApiKey())
     d = json.loads(request.text)
     return d["n"]
@@ -62,103 +85,97 @@ def updateChamps(version):
     if(f[1] == version):
         print("champs.txt version up to date")
     else:
+        API_CALL_LIST.makeCall()
         request = requests.get("http://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/champion.json" + getApiKey())
         d = json.loads(request.text)
-        saveFile("champs.txt",champInfo,champVersion)
+        saveFile("champs.txt",d,version)
         print("champs.txt updated")
+        
+def updateItems(version):
+    f = loadFile("items.txt")
+    if(f[1] == version):
+        print("items.txt version up to date")
+    else:
+        API_CALL_LIST.makeCall()
+        request = requests.get("http://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/item.json" + getApiKey())
+        d = json.loads(request.text)
+        saveFile("items.txt",d,version)
+        print("items.txt updated")
+        
+def updateSpells(version):
+    f = loadFile("spells.txt")
+    if(f[1] == version):
+        print("spells.txt version up to date")
+    else:
+        API_CALL_LIST.makeCall()
+        request = requests.get("http://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/summoner.json" + getApiKey())
+        d = json.loads(request.text)
+        saveFile("spells.txt",d,version)
+        print("spells.txt updated")
 
 def updateConstants():
-    version = getVersion()
-
-
-"""
-versionInfo = json.loads(requests.get("https://ddragon.leagueoflegends.com/realms/na.json" + api_key).text)["n"]
-champVersion = versionInfo["champion"]
-itemVersion = versionInfo["item"]
-ssVersion = versionInfo["summoner"]
-
-champInfo = {}
-loadedInfo = loadFile("champs.txt")
-if(loadedInfo[0] == champVersion):
-    champInfo = loadedInfo[1]
-    print("champs.txt version up to date")
-else:
-    champInfo = json.loads(requests.get("http://ddragon.leagueoflegends.com/cdn/" + champVersion + "/data/en_US/champion.json" + api_key).text)
-    saveInfo("champs.txt",champInfo,champVersion)
-    print("champs.txt updated")
+    versions = getVersion()
+    updateChamps(versions["champion"])
+    updateItems(versions["item"])
+    updateSpells(versions["summoner"])
     
-itemInfo = {}
-loadedInfo = loadFile("items.txt")
-if(loadedInfo[0] == itemVersion):
-    itemInfo = loadedInfo[1]
-    print("items.txt version up to date")
-else:
-    itemInfo = json.loads(requests.get("http://ddragon.leagueoflegends.com/cdn/" + itemVersion + "/data/en_US/item.json" + api_key).text)
-    print("items.txt updated")
-    
-ssInfo = {}
-loadedInfo = loadFile("summonerSpells.txt")
-if(loadedInfo[0] == itemVersion):
-    ssInfo = loadedInfo[1]
-    print("summonerSpells.txt version up to date")
-else:
-    ssInfo = json.loads(requests.get("http://ddragon.leagueoflegends.com/cdn/" + itemVersion + "/data/en_US/summoner.json" + api_key).text)
-    saveInfo("summonerSpells.txt",ssInfo,ssVersion) 
-    print("summonerSpells.txt updated")
-    
-loadedInfo = loadFile("ssId.txt")
-ssDict = {}
-if(loadedInfo[0] == champVersion):
-    ssDict = loadedInfo[1]
-    print("champId.txt version up to date")
-else:
-    for ss in ssInfo["data"]:
-        ssDict[ssInfo["data"][ss]["key"]] = ssInfo["data"][ss]["name"]
-    saveInfo("ssId.txt",ssDict,ssVersion)
-    print("ssId.txt updated")
-    champDict = loadFile("ssId.txt")[1]
-    
-loadedInfo = loadFile("champId.txt")
-champDict = {}
-if(loadedInfo[0] == champVersion):
-    champDict = loadedInfo[1]
-    print("champId.txt version up to date")
-else:
-    for champ in champInfo["data"]:
-        champDict[champInfo["data"][champ]["key"]] = champInfo["data"][champ]["name"]
-    saveInfo("champId.txt",champDict,champVersion)
-    print("champId.txt updated")
-    champDict = loadFile("champId.txt")[1]
+def getAccountByName(name):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + getApiKey())
+    d = json.loads(request.text)
+    return d
 
-loadedInfo = loadFile("SRitems.txt")
-itemDict = {}
-if(loadedInfo[0] == itemVersion):
-    itemDict = loadedInfo[1]
-    print("SRitems.txt version up to date")
-else:
-    for itemNum in itemInfo["data"]:
-        item = itemInfo["data"][itemNum]
-        if(item["maps"]["11"] == True and item["gold"]["purchasable"]):
-            itemDict[item["name"]] = itemNum
-    itemDict.pop("Archangel's Staff (Quick Charge)")
-    itemDict.pop("Broken Stopwatch")
-    itemDict.pop("Entropy Field")
-    itemDict.pop("Flash Zone")
-    itemDict.pop("Manamune (Quick Charge)")
-    itemDict.pop("Port Pad")
-    itemDict.pop("Rod of Ages (Quick Charge)")
-    itemDict.pop("Shield Totem")
-    itemDict.pop("Siege Ballista")
-    itemDict.pop("Siege Refund")
-    itemDict.pop("Tear of the Goddess (Quick Charge)")
-    itemDict.pop("Tower: Beam of Ruination")
-    itemDict.pop("Tower: Storm Bulwark")
-    itemDict.pop("Vanguard Banner")
-    tempDict = {}
-    for key in itemDict:
-        tempDict[itemDict[key]] = key
-    itemDict = tempDict
-    saveInfo("SRitems.txt",itemDict,itemVersion)
-    print("SRitems.txt updated")
-    itemDict = loadFile("SRitems.txt")[1]
-"""
+def getAccountByAccId(accId):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-account/" + accId + getApiKey())
+    d = json.loads(request.text)
+    return d
+
+def getAccountByPPUID(ppuid):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-ppuid/" + ppuid + getApiKey())
+    d = json.loads(request.text)
+    return d
+
+def getAccountBySummId(summId):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/" + summId + getApiKey())
+    d = json.loads(request.text)
+    return d
+
+def getMatchList(accId,queries):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accId + getApiKey() + queries)
+    d = json.loads(request.text)
+    return d
+
+def getMatchListByName(name,queries):
+    summoner = getAccountByName(name)
+    return getMatchList(summoner["accountId"],queries)
+
+def getMatch(matchId):
+    API_CALL_LIST.makeCall()
+    request = requests.get("https://na1.api.riotgames.com/lol/match/v4/matches/" + (str)(matchId) + getApiKey())
+    d = json.loads(request.text)
+    return d
+
+def getAllMatches(matchList):
+    matches = []
+    count = 0
+    for match in matchList["matches"]:
+        m = getMatch(match["gameId"])
+        matches.append(m)
+        print("Got match #" + (str)(count))
+        count += 1
+    return matches
+
+def getLast100GamesByName(name,queries):
+    matchList = getMatchListByName(name,queries)
+    return getAllMatches(matchList)
+
+updateConstants()
+ahir = getLast100GamesByName("CrusherCake","")
+saveFile("ahir.txt",ahir,-1)
+print("doing denis")
+denis = getLast100GamesByName("Avoxin","")
+saveFile("denis.txt",denis,-1)
